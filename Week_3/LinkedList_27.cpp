304b9e3a2b77c6f8f78ed9ecc21b579e9c0830f2
Niranjani Balakumar <63302910+niranjani999@users.noreply.github.com>
2021-04-15 23:44:54 +0530

    Thursday

---
+++ Week_3/LinkedList_27.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+    public:
+    Node *rearrangeEvenOdd(Node *head)
+    {
+       // Your Code here
+        if (head == NULL) 
+        return NULL; 
+        Node *odd = head; 
+        Node *even = head->next;  
+        Node *evenFirst = even; 
+      
+        while (1) 
+        { 
+            if (!odd || !even || !(even->next)) 
+            { 
+                odd->next = evenFirst; 
+                break; 
+            }  
+            odd->next = even->next; 
+            odd = even->next;  
+            if (odd->next == NULL) 
+            { 
+                even->next = NULL; 
+                odd->next = evenFirst; 
+                break; 
+            } 
+            even->next = odd->next; 
+            even = odd->next; 
+        } 
+  
+        return head;
+           
+    }
+};
