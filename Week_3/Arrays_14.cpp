02e78f73eba726aed262d418a7622a604744af18
Niranjani Balakumar <63302910+niranjani999@users.noreply.github.com>
2021-04-16 10:54:59 +0530

    Thursday

---
+++ Week_3/Arrays_14.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        bool con = false;
+        for(int x: nums){
+            if(x==1){
+                con = true;
+                break;
+            }
+        }
+        if(!con) return 1;
+        int n = nums.size();
+        if(n==1) return 2;
+        for(int i=0;i<n;++i)
+            if(nums[i]<=0 || nums[i]>n) nums[i] = 1;
+        for(int i=0;i<n;++i){
+            int x = abs(nums[i]);
+            if(nums[x-1]>0) nums[x-1]*=-1;
+        }
+        for(int i=0;i<n;++i)
+            if(nums[i]>0) return i+1;
+        return n+1;
+    }
+};
