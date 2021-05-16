514395ea99790fa6cad5c817a259f6756e638ee1
Niranjani Balakumar <63302910+niranjani999@users.noreply.github.com>
2021-05-16 12:24:37 +0530

    Sunday

---
+++ Week_4/Arrays_22.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        if(!n) return false;
+        int m = matrix[0].size();
+        int low = 0;
+        int high = (n*m)-1;
+        while(low<= high){
+            int mid = (low+high)/2;
+            if(matrix[mid/m][mid%m] == target) return true;
+            if(matrix[mid/m][mid%m] < target) low = mid+1;
+            else high = mid-1;
+        }
+        return false;
+    }
+};
